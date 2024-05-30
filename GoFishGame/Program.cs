0a55cf4fce1deae06a1389a02bd074ef304ab685
Harsh Singh <kmrharsh50@gmail.com>
2024-05-30 22:50:58 +0530

    [Harsh]Added: calling all method in main method

--- GoFishGame/Program.cs
+++ GoFishGame/Program.cs
@@ -10,6 +10,13 @@ namespace GoFishGame
             Console.Write("Please enter your name: ");
             string playerName = Console.ReadLine();
 
+            Player player = new Player(playerName);
+            Player computer = new Player("Computer");
+            Deck deck = new Deck();
+            deck.Shuffle();
+
+            Game game = new Game(player, computer, deck);
+            game.Start();
         }
     }
 }
