a04ea30fae86308bdebd403654b224ce137cb4aa
Harsh Singh <kmrharsh50@gmail.com>
2024-05-31 00:09:55 +0530

    [Harsh]Added:  To test shufflig of cards

--- GoFishTest/GameTests.cs
+++ GoFishTest/GameTests.cs
@@ -83,5 +83,37 @@ namespace GoFishTest
 
             Assert.AreEqual(player, winner);
         }
+
+        //
+        [TestMethod]
+        public void TestShuffle()
+        {
+            Deck deck = new Deck();
+            List<Card> originalOrder = new List<Card>(deck.Cards);
+
+            deck.Shuffle();
+            List<Card> shuffledOrder = deck.Cards;
+
+            // Check the number of cards is the same
+            Assert.AreEqual(originalOrder.Count, shuffledOrder.Count, "The number of cards should remain the same after shuffling.");
+
+            // Check all cards are still present
+            foreach (Card card in originalOrder)
+            {
+                Assert.IsTrue(shuffledOrder.Contains(card), $"The card {card} should be present after shuffling.");
+            }
+
+            // Check that the order has changed
+            bool orderChanged = false;
+            for (int i = 0; i < originalOrder.Count; i++)
+            {
+                if (originalOrder[i] != shuffledOrder[i])
+                {
+                    orderChanged = true;
+                    break;
+                }
+            }
+            Assert.IsTrue(orderChanged, "The order of cards should change after shuffling.");
+        }
     }
 }
