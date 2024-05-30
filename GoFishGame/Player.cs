211e446614b04d91a8a87f47fa3b34938b5824ab
Harsh Singh <kmrharsh50@gmail.com>
2024-05-30 22:21:40 +0530

    [Harsh]Added: hasRank and Showhand implementation

--- GoFishGame/Player.cs
+++ GoFishGame/Player.cs
@@ -10,6 +10,7 @@ namespace GoFishGame
     {
         public string Name { get; }
         public List<Card> Hand { get; }
+        public int Sets { get; private set; }
 
         public Player(string name)
         {
@@ -17,13 +18,37 @@ namespace GoFishGame
             Hand = new List<Card>();
         }
 
-        public void DrawCard(Deck deck)
+        public void TakeCards(Player opponent, string rank)
         {
-            Card card = deck.Draw();
-            if (card != null)
+            List<Card> cardsToTake = opponent.Hand.Where(card => card.Rank == rank).ToList();
+            Hand.AddRange(cardsToTake);
+            opponent.Hand.RemoveAll(card => card.Rank == rank);
+
+            if (Hand.Count(card => card.Rank == rank) == 4)
             {
-                Hand.Add(card);
+                Sets++;
+                Hand.RemoveAll(card => card.Rank == rank);
+                Console.WriteLine($"{Name} completed a set of {rank}s!");
             }
+        }
+
+        public bool HasRank(string rank)
+        {
+            return Hand.Exists(card => card.Rank == rank);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Player other = (Player)obj;
+            return Name == other.Name;
+        }
+
+        public string ShowHand()
+        {
+            return string.Join(", ", Hand.Select(card => card.ToString()));
         }
     }
 }
