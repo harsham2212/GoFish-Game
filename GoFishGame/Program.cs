31ec43f937b2847fa382d5c2b2e4775a3d52b6b2
Harsh Singh <kmrharsh50@gmail.com>
2024-05-30 22:06:16 +0530

    [Harsh]Added: main program to run.

--- GoFishGame/Program.cs
+++ GoFishGame/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System.Numerics;
+
+namespace GoFishGame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Go Fish!");
+            Console.Write("Please enter your name: ");
+            string playerName = Console.ReadLine();
+
+        }
+    }
+}
