bc843d731a32e64835202234344ae77ac7e33956
Harsh Singh <kmrharsh50@gmail.com>
2024-05-30 21:41:51 +0530

    [Harsh]Added: Single card with rank and suit.

---
+++ GoFishGame/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoFishGame
+{
+    public class Card
+    {
+        public string Rank { get; }
+        public string Suit { get; }
+
+        public Card(string rank, string suit)
+        {
+            Rank = rank;
+            Suit = suit;
+        }
+
+        public override string ToString()
+        {
+            return $"{Rank} of {Suit}";
+        }
+    }
+}
---
+++ GoFishTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace GoFishTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
