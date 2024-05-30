f70315c5a04d5f05bd924d1178428069398ff3c2
Harsh Singh <kmrharsh50@gmail.com>
2024-05-30 22:16:52 +0530

    [Harsh]Added: Implemetatio of players ad drawcard

---
+++ GoFishGame/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoFishGame
+{
+    public class Player
+    {
+        public string Name { get; }
+        public List<Card> Hand { get; }
+
+        public Player(string name)
+        {
+            Name = name;
+            Hand = new List<Card>();
+        }
+
+        public void DrawCard(Deck deck)
+        {
+            Card card = deck.Draw();
+            if (card != null)
+            {
+                Hand.Add(card);
+            }
+        }
+    }
+}
